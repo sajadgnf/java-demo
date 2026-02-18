f9f9502d4e70266d42517ddf1177bbd9ea0646f9
s.ganjifar <sajadganjifar@gmail.com>
2026-02-18 06:27:00 +0330

    Enhance User entity with email, address management, profile association, and wishlist functionality

--- src/main/java/com/example/demo/enitities/User.java
+++ src/main/java/com/example/demo/enitities/User.java
@@ -1,5 +1,8 @@
 package com.example.demo.enitities;
 
+import java.util.List;
+import java.util.Set;
+
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,10 +16,36 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-
     @Column(name = "name")
     private String name;
 
+    @Column(name = "email")
+    private String email;
+
     @Column(name = "password")
     private String password;
+
+    @OneToMany(mappedBy = "user", cascade = { CascadeType.PERSIST, CascadeType.REMOVE }, orphanRemoval = true)
+    private List<Address> addresses = new ArrayList<>();
+
+    public void addAddress(Address address) {
+        addresses.add(address);
+        address.setUser(this);
+    }
+
+    public void removeAddress(Address address) {
+        addresses.remove(address);
+        address.setUser(null);
+    }
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.REMOVE)
+    private Profile profile;
+
+    @ManyToMany
+    @JoinTable(name = "wishlist", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "product_id"))
+    private Set<Product> wishlist = new HashSet<>();
+
+    public void addToWishlist(Product product) {
+        wishlist.add(product);
+    }
 }
